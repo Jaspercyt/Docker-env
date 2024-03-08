21a912eca83dc075bea35b4bd9bc1150e4ec0427
Jaspercyt <88648972+Jaspercyt@users.noreply.github.com>
2024-03-08 20:20:41 +0800

    Add files via upload

--- docker-install.sh
+++ docker-install.sh
@@ -12,34 +12,34 @@ done
 echo "所有衝突套件卸載完成。"
 
 echo "[TASK 3] 卸載舊版本的 Docker..."
-apt-get -y purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
-rm -rf /var/lib/docker
-rm -rf /var/lib/containerd
+sudo apt-get -y purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
 echo "舊版本的 Docker 卸載完成。"
 
 echo "[TASK 4] 設定 Docker 的 apt 儲存庫..."
 # 加入 Docker 官方的 GPG 密鑰：
-apt-get -y update
-apt-get -y install ca-certificates curl gnupg
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # 加入儲存庫到 Apt 來源：
-echo \
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+sudo apt-get update
 echo "Docker 的 apt 儲存庫設定完成。"
 
 echo "[TASK 5] 安裝 Docker 套件..."
-apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "Docker 套件安裝完成。"
 
 echo "[TASK 6] 安裝自動完成腳本..."
-apt-get update
-apt-get install bash-completion -y
-curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
+sudo apt-get update
+sudo apt-get install bash-completion -y
+sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
 echo "自動完成腳本安裝完成。"
 
 echo "[TASK 7] 啟動並啟用 Docker..."
